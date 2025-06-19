e187c48e393614ebc9931faae51eea3d47ab9605
subharthy kuiry <subharthykuiry@gmail.com>
2025-06-19 13:08:20 +0530

    initial-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import AddBlog from './Pages/Admin/AddBlog.jsx'
 import ListBlog from './Pages/Admin/ListBlog.jsx'
 import Comments  from './Pages/Admin/Comments.jsx'
 // App.jsx
-import Login from './Components/Admin/login.jsx' // ✅ case must match file system
+import Login from "./components/admin/Login.jsx";
 import 'quill/dist/quill.snow.css'; // ✅ Important: must be imported
 import {Toaster} from 'react-hot-toast'
 import { useAppContext } from '../context/AppContext.jsx'
